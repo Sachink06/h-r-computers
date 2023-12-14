a39862df44963eb80bb6e51dc99c1a29f99863a6
Sachink06 <sachinkatre06@gmail.com>
2023-12-14 20:35:08 +0530

    add

---
+++ script.js
@@ -0,0 +1,87 @@
+function cursorEffect() {
+    var page1Content = document.querySelector("#page1-content");
+var cursor = document.querySelector("#cursor");
+
+page1Content.addEventListener("mousemove",function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y
+    })
+});
+
+page1Content.addEventListener("mouseenter",function(){
+    gsap.to(cursor,{
+        scale:1,
+        opacity:1
+    })
+})
+
+page1Content.addEventListener("mouseleave",function(){
+    gsap.to(cursor,{
+        scale:0,
+        opacity:0
+    })
+})
+}
+
+cursorEffect();
+
+
+function page2Animation() {
+    gsap.from("#page2 h2",{
+        y:120,
+        stagger:0.25,
+        duration:1,
+        ScrollTrigger:"#page2 h2"
+    })
+}
+
+page2Animation()
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: true,
+          }
+      });
+}
+
+swiperAnimation()
+
+var tl = gsap.timeline()
+
+tl.from("#loader h3", {
+    x: 100,
+    opacity:0,
+    duration:1,
+    stagger: 0.1,
+})
+
+tl.to("#loader h3", {
+    opacity: 0,
+    x: -20,
+    stagger:0.1,
+    duration:1,
+})
+
+tl.to("#loader", {
+    opacity:0,
+
+})
+
+tl.from("#page1-content h1 span", {
+    y:100,
+    opacity:0,
+    stagger:0.1,
+    delay:-0.5
+})
+
+tl.to("#loader", {
+    display: "none"
+    
+})
+
